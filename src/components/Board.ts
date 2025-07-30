1ef34e323c0589c0bff9beb65b64d05a952c4e28
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-07-29 21:23:34 -0300

    added: class board

---
+++ src/components/Board.ts
@@ -0,0 +1,39 @@
+import { Cell } from './Cell';
+import type { Position } from '../models/types';
+
+export class Board {
+  private cells: Cell[][];
+  private container: HTMLDivElement;
+
+  constructor(onCellClick: (pos: Position) => void) {
+    this.container = document.getElementById('game') as HTMLDivElement;
+    this.cells = [];
+
+    // Создаем сетку ячеек
+    for (let row = 0; row < 3; row++) {
+      const cellRow: Cell[] = [];
+      for (let col = 0; col < 3; col++) {
+        const position: Position = { row, col };
+        const cell = new Cell(position, onCellClick);
+        cellRow.push(cell);
+        this.container.appendChild(cell.getElement());
+      }
+      this.cells.push(cellRow);
+    }
+  }
+
+  // Возвращает язычейку по позиции
+  public getCell(position: Position): Cell {
+    return this.cells[position.row][position.col];
+  }
+
+  // Получение всех ячеек
+  public getAllCells(): Cell[][] {
+    return this.cells;
+  }
+
+  // Очищение всех ячеек
+  public clear(): void {
+    this.cells.forEach(row => row.forEach(cell => cell.clear()));
+  }
+}
